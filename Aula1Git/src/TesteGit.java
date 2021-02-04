47ee6cadf6f70187b73a4d0dccb0ec498e705ae1
vaio <celiosilvaa@gmail.com>
2021-02-04 10:19:32 -0300

    Segunda modificação

--- Aula1Git/src/TesteGit.java
+++ Aula1Git/src/TesteGit.java
@@ -4,6 +4,7 @@ public class TesteGit {
 	public static void main(String[] args) {
 		System.out.println("Hello World  !!");
 		System.out.println("Alteração feita em 04/02/2021");
+		System.out.println("Segunda alteração");
 	}
 
 }
